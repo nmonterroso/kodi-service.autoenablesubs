3f1c4af3557b1dcb0b26ca1ef16a1dd00ee59cb2
nmonterroso <nmonterroso@users.noreply.github.com>
2018-01-26 16:12:36 -0800

    enumerate to find the proper index

--- resources/lib/player.py
+++ resources/lib/player.py
@@ -9,6 +9,9 @@ class AutoSubEnablePlayer(xbmc.Player):
         xbmc.Player.__init__(self)
         self.logger = kwargs['logger']
 
+    def get_preferred_lang(self):
+        return 'eng'
+
     def get_active_player_id(self):
         active_player = kodiutils.jsonrpc(1, {
             'method': 'Player.GetActivePlayers'
@@ -23,44 +26,45 @@ class AutoSubEnablePlayer(xbmc.Player):
                 'playerid': player_id,
                 'properties': [
                     'currentaudiostream',
-                    'subtitleenabled',
-                    'currentsubtitle',
-                    # 'subtitles'
+                    'subtitleenabled'
                 ]
             }
         })
 
         return (
             details['currentaudiostream']['language'],
-            details['subtitleenabled'],
-            details['currentsubtitle']['index']
+            details['subtitleenabled']
         )
 
-    def needs_subs(self, current_language):
-        # TODO: get from a setting
-        return not current_language == 'eng'
+    def needs_subs(self, current_lang, preferred_lang):
+        return current_lang != preferred_lang
 
-    def get_desired_sub_index(self):
-        return 0
-        # return next((x for x in self.getAvailableSubtitleStreams() if x == 'eng'), None)
+    def get_preferred_lang_sub_index(self, preferred_lang):
+        for index, lang in enumerate(self.getAvailableSubtitleStreams()):
+            self.logger.debug("preferred_lang %s sub lang %s", preferred_lang, lang)
+            if lang == preferred_lang:
+                return index
+
+        return None
 
     def onPlayBackStarted(self):
         if self.isPlayingVideo():
             active_player_id = self.get_active_player_id()
-            (current_audio_lang, is_sub_enabled, current_sub_index) = self.get_stream_details(active_player_id)
+            preferred_lang = self.get_preferred_lang()
+            (current_audio_lang, is_sub_enabled) = self.get_stream_details(active_player_id)
 
             # subtitles already on, or language is already the user desired language
-            if is_sub_enabled or not self.needs_subs(current_audio_lang):
+            if is_sub_enabled or not self.needs_subs(current_audio_lang, preferred_lang):
                 self.logger.debug("subs not needed")
                 return
 
-            desired_sub_index = self.get_desired_sub_index()
-            if desired_sub_index is None:
+            sub_index = self.get_preferred_lang_sub_index(preferred_lang)
+            if sub_index is None:
                 self.logger.debug("desired sub index not found :(")
                 return
 
-            self.logger.debug("desired sub index %s", desired_sub_index)
-            self.setSubtitleStream(desired_sub_index)
+            self.logger.debug("desired sub index %s", sub_index)
+            self.setSubtitleStream(sub_index)
             self.showSubtitles(True)
 
             # seek to beginning, since there is often a delay in subs turning on (keyframes?)
